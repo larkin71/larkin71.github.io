d5a34a921543780ca11719064db2b443d560fb1e
larkin71 <JakeLarkin@Jake-Larkins-MacBook-Pro.local>
2014-07-09 20:41:21 -0500

    fasd

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("body").queryLoader2({
-        barColor: "#326ea9",
-        backgroundColor: "#5a5a5a",
+        barColor: "#fff",
+        backgroundColor: "#326ea9",
         percentage: true,
         barHeight: 1,
         completeAnimation: "fade",
