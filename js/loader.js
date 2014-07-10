ff893324602bc432ef8c0b5ff8a2688e30d284b3
larkin71 <JakeLarkin@Jake-Larkins-MacBook-Pro.local>
2014-07-09 20:48:02 -0500

    loader

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
     $("body").queryLoader2({
         barColor: "#f5f6f0",
-        backgroundColor: "#fff",
+        backgroundColor: "#5a5a5a",
         percentage: true,
         barHeight: 1,
         completeAnimation: "grow",
         minimumTime: 50
     });
+});
+window.addEventListener('DOMContentLoaded', function() {
+    $("body").queryLoader2();
 });
