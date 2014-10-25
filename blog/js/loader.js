948a450a0ab82e34f0f006a524b57288ff44737a
larkin71 <larkin.71@osu.edu>
2014-10-24 23:45:45 -0400

    check

---
+++ blog/js/loader.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#f5f6f0",
+        backgroundColor: "#5a5a5a",
+        percentage: true,
+        barHeight: 5,
+        completeAnimation: "grow",
+        minimumTime: 10
+    });
+});
